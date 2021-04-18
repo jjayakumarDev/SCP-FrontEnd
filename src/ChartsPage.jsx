772752c2b02df3980e0ef31e87768d1747bd3280
jjayakumarDev <x20154810@student.ncirl.com>
2021-04-18 07:09:44 +0100

    updated chart static to live update from db

--- src/ChartsPage.jsx
+++ src/ChartsPage.jsx
@@ -6,11 +6,18 @@ import Services from "./services";
 
 class ChartsPage extends React.Component {
   state = {
-    dataPie: {
+    eyeBlink : '0',
+    distraction : '0',
+    confidence : '1',
+    dataPie: ''
+  }
+
+  componentDidMount(){
+    let pie = {
       labels: ["Eye Rest Level", "Distraction Level", "Confident Level"],
       datasets: [
         {
-          data: [234 , 346, 346],
+          data: [this.state.eyeBlink , this.state.distraction, this.state.confidence],
           backgroundColor: [
             "#F7464A",
             "#949FB1",
@@ -26,14 +33,22 @@ class ChartsPage extends React.Component {
         }
       ]
     }
+    this.setState({dataPie : pie})
   }
 
   render() {
+
     const handleGetResult = () => {
       var date = new Date();
       let id = date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear();
       Services.getFaceDetectionData(id).then((res) => {
         let result = res.data;
+        if(result){
+          const faceinfo = result.faceInfo.M;
+          this.setState({eyeBlink : faceinfo.eyeBlink.N})
+          this.setState({distraction : faceinfo.distraction.N})
+          this.setState({confidence : faceinfo.confidence.N})
+        }
         console.log(result);
       });
       };
@@ -44,25 +59,44 @@ class ChartsPage extends React.Component {
                 <div class="col-md-3">
                     <div class="card border-info mx-sm-1 p-3">
                         <div class="text-info text-center mt-3"><h4>Eye Rest Level</h4></div>
-                        <div class="text-info text-center mt-2"><h1>234</h1></div>
+                        <div class="text-info text-center mt-2"><h1>{this.state.eyeBlink}</h1></div>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="card border-danger mx-sm-1 p-3">
                         <div class="text-danger text-center mt-3"><h4>Distraction Level</h4></div>
-                        <div class="text-danger text-center mt-2"><h1>346</h1></div>
+                        <div class="text-danger text-center mt-2"><h1>{this.state.distraction}</h1></div>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="card border-warning mx-sm-1 p-3">
                         <div class="text-warning text-center mt-3"><h4>Confident Level</h4></div>
-                        <div class="text-warning text-center mt-2"><h1>346</h1></div>
+                        <div class="text-warning text-center mt-2"><h1>{this.state.confidence}</h1></div>
                     </div>
                 </div>
             </div>
       <MDBContainer>
         <h3 className="mt-5">Report</h3>
-        <Pie data={this.state.dataPie} options={{ responsive: true }} />
+        <Pie data={{
+      labels: ["Eye Rest Level", "Distraction Level", "Confident Level"],
+      datasets: [
+        {
+          data: [this.state.eyeBlink , this.state.distraction, this.state.confidence],
+          backgroundColor: [
+            "#F7464A",
+            "#949FB1",
+            "#4D5360",
+            "#AC64AD"
+          ],
+          hoverBackgroundColor: [
+            "#FF5A5E",
+            "#A8B3C5",
+            "#616774",
+            "#DA92DB"
+          ]
+        }
+      ]
+    }} options={{ responsive: true }} />
       </MDBContainer>
       </div>
     );
